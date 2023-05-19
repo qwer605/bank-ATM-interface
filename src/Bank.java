683cff003a358b1e5c1ace9f0f582650cf333286
qwer605 <tolockoyegor@gmail.com>
2023-05-19 12:38:28 +0300

    Setting bank data

--- src/Bank.java
+++ src/Bank.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+
 public class Bank {
+
+    /*
+    The name of this bank
+     */
+    private String name;
+
+    /*
+    The list of users
+     */
+    private ArrayList<User> users;
+
+    /*
+    The list of accounts
+     */
+    private ArrayList<Account> accounts;
+
 }

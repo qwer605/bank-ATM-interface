f8735d4f44f7884bb81ffc9de240f0cc575d25c3
qwer605 <tolockoyegor@gmail.com>
2023-05-19 12:21:47 +0300

    Setting user`s data

--- src/User.java
+++ src/User.java
@@ -1,2 +1,31 @@
+import java.util.ArrayList;
+
 public class User {
+
+    /*
+    The first name of the user
+     */
+    private String firstname;
+
+    /*
+    The last name of the user
+     */
+    private String lastname;
+
+    /*
+    The ID number of the user
+     */
+    private String uuid;
+
+    /*
+    The MD5 hash of the user`s pin number
+     */
+    private byte pinHash[];
+
+    /*
+    The list of accounts for this user
+     */
+    private ArrayList<Account> accounts;
+
+
 }

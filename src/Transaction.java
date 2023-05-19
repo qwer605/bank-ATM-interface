26cb7fbd5dc5b77e4e6c7474ee2dbd64f30f64bc
qwer605 <tolockoyegor@gmail.com>
2023-05-19 12:34:10 +0300

    Setting transaction data

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,2 +1,26 @@
+
+import java.util.Date;
+
 public class Transaction {
+
+    /*
+    The amount of this transaction
+     */
+    private double amount;
+
+    /*
+    The time and date of this transaction
+     */
+    private Date timestamp;
+
+    /*
+    The memo for this transaction
+     */
+    private String memo;
+
+    /*
+    The account in which transaction was performed
+     */
+    private Account inAccount;
+
 }
